71bd143277a31c329ed2faadeddd4496ee8310e4
kacperborowieckb <kacperborowiec.kb@gmail.com>
2026-01-03 18:19:14 +0100

    feat: add models and db connection

--- HotelReservationSystem/Data/ApplicationDbContext.cs
+++ HotelReservationSystem/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using HotelReservationSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotelReservationSystem.Data
@@ -8,6 +9,31 @@ namespace HotelReservationSystem.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomType> RoomTypes { get; set; }
+        public DbSet<Guest> Guests { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Room>()
+                .HasOne(r => r.RoomType)
+                .WithMany(rt => rt.Rooms)
+                .HasForeignKey(r => r.RoomTypeId);
+
+            modelBuilder.Entity<Reservation>()
+                .HasOne(res => res.Room)
+                .WithMany()
+                .HasForeignKey(res => res.RoomId);
+
+            modelBuilder.Entity<Reservation>()
+                .HasOne(res => res.Guest)
+                .WithMany(g => g.Reservations)
+                .HasForeignKey(res => res.GuestId);
         }
     }
 }
---
+++ HotelReservationSystem/Models/Models.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservationSystem.Models
+{
+    public class RoomType
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        public decimal BasePrice { get; set; }
+
+        public virtual ICollection<Room>? Rooms { get; set; }
+    }
+
+    public class Room
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Room Number")]
+        public required string RoomNumber { get; set; }
+
+        public int RoomTypeId { get; set; }
+
+        [Required]
+        public required virtual RoomType RoomType { get; set; }
+
+        public bool IsAvailable { get; set; }
+    }
+
+    public class Guest
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public required string FirstName { get; set; }
+
+        [Required]
+        public required string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        public virtual ICollection<Reservation>? Reservations { get; set; }
+    }
+
+    public class Reservation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        public int RoomId { get; set; }
+        public virtual Room? Room { get; set; }
+
+        public int GuestId { get; set; }
+        public virtual Guest? Guest { get; set; }
+    }
+}
